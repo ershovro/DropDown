4a3e3a301a091fc3b825569e4611d6cb9825dbd3
ro.ershov <ro.ershov@tensor.ru>
2019-10-01 16:21:59 +0300

    webpack config init

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const 
+   webpack = require('webpack'),
+   NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = {
+   mode: NODE_ENV,
+   context: __dirname + '/frontend',
+   entry: './app.js',
+   output: {
+      path: __dirname + '/public',
+      publicPath: '/',
+      filename: 'bundle.js',
+      library: '[name]'
+   },
+   devtool: NODE_ENV === 'development' ? 'cheap-inline-module-source-map' : 'none',
+   watch: NODE_ENV === 'development',
+   watchOptions: {
+      ignored: /node_modules/
+   }   
+};

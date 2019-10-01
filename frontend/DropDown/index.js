8a2f8bc41238d1a57d06a0203b2fe4d77b1f4896
ro.ershov <ro.ershov@tensor.ru>
2019-10-01 23:08:28 +0300

    intermediate commit

--- frontend/DropDown/index.js
+++ frontend/DropDown/index.js
@@ -1,18 +1,24 @@
 import './DropDown.css';
 
 const template = (control) =>
-   `    
-      <span class="DropDown__label">${control.label}</span>
-      <span></span>
-      <span class="dropdown__arrowContainer">
-         <svg
-            class="dropdown__arrow"
-            width="10"
-            height="5"
-            viewBox="0 0 10 5"
-            fill-rule="evenodd">          
-               <path d="M10 0L5 5 0 0z"></path>
-         </svg>
+   `  <span class="DropDown__input">  
+         <span class="DropDown__input-label">${control.label}</span>
+         <span class="DropDown__input-value"></span>
+         <span class="DropDown__input-arrowContainer">
+            <svg
+               class="DropDown__input-arrow"
+               width="10"
+               height="5"
+               viewBox="0 0 10 5"
+               fill-rule="evenodd"> 
+                  <title>Открыть меню</title>
+                  <path d="M10 0L5 5 0 0z"></path>
+            </svg>
+         </span>    
+      </span>
+       
+      <span class="dropdown__list">
+         ${control.items.map(item => `<span class="dropdown__list-item">${item.value}</span>`).join('')}
       </span>
    `
 
@@ -24,7 +30,7 @@ export default class DropDown {
    } = {}) {   
       this.label = label;
       this.placeHolder = placeHolder;
-      this.items = items;
+      this.items = [...items].map(item => ({value: item, isSelected: false}));
       this.elem = this.createElement();     
    }
    

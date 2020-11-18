e95beac25794a9bf512fc2928bd0ce632562b2ce
Roman Ershov <romma4893@gmail.com>
2020-11-18 12:47:18 +0300

    feature

--- frontend/app.js
+++ frontend/app.js
@@ -4,5 +4,5 @@ let dropDown = new DropDown({
    label: 'Menu',
    items: ['1 item', '2 item', '3 item']
 });
-
+dropDown.elem.cssStyle = 'margin: auto;';
 document.body.appendChild(dropDown.elem);
